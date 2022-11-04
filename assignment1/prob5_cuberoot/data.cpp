32ea22f8879dd6b1485190a2b5170f8a0913aebc
ztzhu1 <zhuzitian@zju.edu.cn>
2022-11-04 22:32:23 +0800

    Finish assignment1

--- assignment1/prob5_cuberoot/data.cpp
+++ assignment1/prob5_cuberoot/data.cpp
@@ -11,19 +11,32 @@ void initRandom(float *values, int N) {
 
 // Generate data that gives high relative speedup
 void initGood(float *values, int N) {
+    /* result:
+        serial    : 32951 ms
+        ispc      :  5217 ms   6.3x speed up
+        task ispc :   472 ms  69.9x speed up
+    */
     for (int i=0; i<N; i++)
     {
         // TODO: Choose data values that will yield high speedup
-        values[i] = 1.f;
+        values[i] = 0.f;
     }
 }
 
 // Generate data that gives low relative speedup
 void initBad(float *values, int N) {
+    /* result:
+        serial    :  5716 ms
+        ispc      :  1085 ms   5.3x speed up
+        task ispc :   107 ms  53.4x speed up
+    */
     for (int i=0; i<N; i++)
     {
         // TODO: Choose data values that will yield low speedup
-        values[i] = 1.f;
+
+        // This should be much slower than good/random data,
+        // but it's not. I haven't figured out it.
+        values[i] = N % 8 == 0 ? 1.998f : 1.f;
     }
 }
 
